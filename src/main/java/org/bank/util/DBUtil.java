d9ce13136559eb30698a4a132be3c9ab8713d104
Manjunath <manjunathsrinivas2005@gmail.com>
2026-03-03 10:39:43 +0530

    Final changes before merging to main

--- src/main/java/org/bank/util/DBUtil.java
+++ src/main/java/org/bank/util/DBUtil.java
@@ -8,6 +8,8 @@ Does NOT contain SQL
  */
 package org.bank.util;
 
+import org.w3c.dom.ls.LSOutput;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
@@ -15,7 +17,7 @@ import java.sql.SQLException;
 public class DBUtil {
     private static final String url = "jdbc:postgresql://localhost:5432/banking-system-pdb-v1";
     private static final String user = "postgres";
-    private static final String password = "@@@manju123";
+    private static final String password = "your_postgres_password";
 
     public static Connection getConnection() throws SQLException{
         return DriverManager.getConnection(url, user, password);
